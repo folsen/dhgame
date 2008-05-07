f4061b579a6fa7fcde46e80a63cdd7a500a7f1f3
Fredrik Olsen <IQue@c-16f4e055.118-1-64736c15.cust.bredbandsbolaget.se>
2008-05-07 13:53:04 +0200

    added capify

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "dhgame"
+
+
+default_run_options[:pty] = true
+set :repository,  "ique@dhgame.eu:/repos/dhgame"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+set :scm, "git"
+set :user, "ique"
+set :branch, "origin/master"
+
+set :deploy_to, "/var/www/dhgame"
+set :use_sudo, false
+
+role :web, "dhgame.eu"
+role :app, "dhgame.eu"
+role :db,  "dhgame.eu", :primary => true
