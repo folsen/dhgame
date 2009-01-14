c75343934a2776770e32331d818b85d51407862b
Fredrik Olsen <ique@3h.se>
2009-01-14 19:29:20 +0100

    added hoptoad exception notifier

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,5 +4,6 @@
 class ApplicationController < ActionController::Base
   # Be sure to include AuthenticationSystem in Application Controller
   include AuthenticatedSystem
+  include HoptoadNotifier::Catcher
   
 end
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = '3b9a0b414b03bba649ec23ed3ceb6639'
+end
